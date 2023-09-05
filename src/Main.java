7552475420280fd99957fd88b1cc14d498171919
Sam Black <sam.black@lubbockisd.org>
2023-09-05 09:03:22 -0500

    notes on identifires and primatives

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // data types -- critical
+        // identifier rules
+        // -- always start with a letter
+        // -- NO spaces
+        // -- CAN use numbers
+        // -- _ is the only symbol allowed
+        // -- CANNOT use Java keywords
+
+        //general conventions
+        // -- classes begin with a Capital
+        // -- camelCase is common for variableNames
+        // -- use names that mean something
+
+        // Data Types
+        // -primatives: store a single, simple value
+        // 8 types: 4 integers, 2 real numbers, 1 boolean (t/f), 1 char
+        // --integers:  byte, short, int, long
+        //      bits:     8     16    32    64
+        //    byte:  -128 to 127
+        //   short:  -32768 to 32767
+        //     int:  -2147483648 to 2147483647
+        //    long:  -4quintillion to 4quintillion
+
     }
 }
